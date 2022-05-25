0f3c79fee37bcb2b6ddf955cdc8b6e2255cc6250
minormending <54426030+minormending@users.noreply.github.com>
2022-05-25 08:16:41 -0400

    run black

--- frenchbee/data.py
+++ frenchbee/data.py
@@ -25,12 +25,15 @@ class FrenchBeeData:
         resp: Response = self.session.get(url)
 
         soup: BeautifulSoup = BeautifulSoup(resp.text, "html.parser")
-        source_list_tag: Tag = soup.find("select", id="edit-visible-newsearch-flights-from")
+        source_list_tag: Tag = soup.find(
+            "select", id="edit-visible-newsearch-flights-from"
+        )
         source_tags: ResultSet = source_list_tag.find_all("option")
         for source_tag in source_tags:
             code: str = source_tag["value"]
             name: str = source_tag.getText()
             yield Airport(code, name)
+
 
 if __name__ == "__main__":
     data = FrenchBeeData()
